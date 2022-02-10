c8a0b010ef2bdf1022c250a89b3a51a7700e6250
alex <alex@tekartik.com>
2022-02-10 16:18:18 +0100

    ci: longer build timeout

--- build_support/test/wear_app_test.dart
+++ build_support/test/wear_app_test.dart
@@ -1,11 +1,12 @@
 import 'package:build_support/wear_app.dart';
 import 'package:test/test.dart';
 
+var buildTimeout = Timeout(Duration(minutes: 10));
 void main() {
   test('buildWearApp', () async {
     await buildWearApp();
-  });
+  }, timeout: buildTimeout);
   test('testWearApp', () async {
     await testWearApp();
-  });
+  }, timeout: buildTimeout);
 }
